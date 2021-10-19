ab75845dbcce71f1ca3db2e2654c4c8405794622
sanskritigupta5 <sanskritigupta0005@gmail.com>
2021-10-19 09:51:27 +0530

    added server url

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-
-/* var username = prompt("give me username");
-alert("Hello!! " + username);
-console.log(btnTranslate);  */
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
-function clickHandler(){
-    console.log("Clicked!");
-    console.log("input", txtInput.value);
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+
+function errorHandler(error){
+  console.log("error occured", error)
+}
+
+function clickHandler() {
+   var inputText = txtInput.value; // taking input
+
+   // calling server for processing
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => { 
+     var translatedText = json.contents.translated
+     outputDiv.innerText = translatedText;
+    
+   
+  })
+  .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickHandler)
